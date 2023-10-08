9a324751fb158087fa1451d913f5e626e0ff71f3
Itsmynool <juancamilo456123@gmail.com>
2023-10-08 17:28:36 -0500

    Sirve

--- sorellaApp/src/app/carrito/carrito.component.ts
+++ sorellaApp/src/app/carrito/carrito.component.ts
@@ -8,14 +8,21 @@ import { CarritoService } from '../carrito.service';
 })
 export class CarritoComponent {
   productosEnCarrito: any[] = [];
+  total: number = 0;
 
   constructor(private carritoService: CarritoService) {
     this.productosEnCarrito = this.carritoService.obtenerProductos();
-    console.log("PRODUCTOS: ", this.productosEnCarrito)
+    for (let i = 0; i < this.productosEnCarrito.length; i++) {
+      this.total += (this.productosEnCarrito[i].producto.precio * this.productosEnCarrito[i].cantidad);
+      // Realiza acciones con el producto aquí
+    }
   }
 
   eliminarDelCarrito(producto: any) {
     this.carritoService.eliminarProducto(producto._id)
+  }
+
+  realizarPago(){
   }
   
 }
